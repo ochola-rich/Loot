575f4664f5468529bda64646bd5f095edc7fa447
riotieno <richardochola3@gmail.com>
2026-06-09 00:25:40 +0300

    refactor: migrate application entry point and config to loot-api

--- loot-api/src/main/java/com/loot/LootApplication.java
+++ loot-api/src/main/java/com/loot/LootApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LootApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LootApplication.class, args);
+		
 	}
 
 }
